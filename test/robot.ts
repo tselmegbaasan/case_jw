618ee2af19ab75f970d62a5ff927b321a91e8418
Tselmeg Baasan <ts2530ba-s@student.lu.se>
2021-04-11 17:33:34 +0200

    first test added

--- test/robot.ts
+++ test/robot.ts
@@ -1,7 +1,13 @@
+import Room from '../src/room';
+import Robot from '../src/robot';
 import { expect } from 'chai';
 
-describe('room', () => {
-  it('should be initialized with inputed values', () => {
-    console.log('hello');
+describe('robot', () => {
+  const room = new Room({ width: 5, height: 5 });
+  const position = { x: 1, y: 2, direction: 'N' };
+  it('should have direction east when turning right', () => {
+    const robot = new Robot(position, room);
+    robot.turnRight();
+    expect(robot.position.direction).to.equal('E');
   });
 });
